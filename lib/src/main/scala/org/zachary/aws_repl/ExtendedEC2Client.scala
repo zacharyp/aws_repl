0f17cb7718a18b6c969cbc979de9343ae8c80950
Zachary Pitts <zacharyp@gmail.com>
2015-01-16 16:52:35 -0800

    tagging ec2 instances, including all by 'Name' tag filter

--- lib/src/main/scala/org/zachary/aws_repl/ExtendedEC2Client.scala
+++ lib/src/main/scala/org/zachary/aws_repl/ExtendedEC2Client.scala
@@ -3,7 +3,9 @@ package org.zachary.aws_repl
 import com.amazonaws.ClientConfiguration
 import com.amazonaws.auth.AWSCredentialsProvider
 import com.amazonaws.services.ec2.AmazonEC2Client
-import com.amazonaws.services.ec2.model.{CreateImageResult, CreateImageRequest, DeregisterImageRequest}
+import com.amazonaws.services.ec2.model._
+
+import scala.collection.JavaConverters._
 
 class ExtendedEC2Client(awscp: AWSCredentialsProvider, cc: ClientConfiguration) extends AmazonEC2Client(awscp, cc) {
 
@@ -16,6 +18,38 @@ class ExtendedEC2Client(awscp: AWSCredentialsProvider, cc: ClientConfiguration) 
     val image: CreateImageResult = createImage(request)
 
     image.getImageId
+  }
+
+  def createTags(instanceId: String, tags: Map[String, String]): Unit = {
+    val request: CreateTagsRequest = new CreateTagsRequest
+    request.setResources(List(instanceId).asJava)
+    request.setTags(tags.map(t => {
+      new Tag(t._1, t._2)
+    }).toList.asJava)
+    createTags(request)
+  }
+
+  def deleteTags(instanceId: String, tags: Map[String, String]): Unit = {
+    val request: DeleteTagsRequest = new DeleteTagsRequest
+    request.setResources(List(instanceId).asJava)
+    request.setTags(tags.map(t => {
+      new Tag(t._1, t._2)
+    }).toList.asJava)
+    deleteTags(request)
+  }
+
+  def createTagsOnInstancesByName(instanceName: String, tagName: String, tagValue: String): Unit = {
+    val filters: Filter = new Filter("tag:Name", List(s"$instanceName").asJava)
+    val request: DescribeInstancesRequest = new DescribeInstancesRequest
+    request.setFilters(List(filters).asJava)
+
+    val instances: DescribeInstancesResult = describeInstances(request)
+    instances.getReservations.iterator().asScala.foreach(reservation => {
+      reservation.getInstances.asScala.foreach(instance => {
+        createTags(instance.getInstanceId, Map(tagName -> tagValue))
+        println(s"Setting tag $tagName for ${instance.getInstanceId}")
+      })
+    })
   }
 
   def deregisterImage(amiId: String): Unit = {
