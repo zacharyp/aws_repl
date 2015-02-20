ea0e21dc91666e6fd3452d82e770c14990c9ce92
Stephen Duncan Jr <stephen.duncan@gmail.com>
2015-02-20 14:55:23 -0800

    Add route53 DNS addition utility

--- lib/src/main/scala/org/zachary/aws_repl/Clients.scala
+++ lib/src/main/scala/org/zachary/aws_repl/Clients.scala
@@ -13,6 +13,7 @@ class Clients(provider: AWSCredentialsProvider, configuration: ClientConfigurati
   val rds = new ExtendedRDSClient(provider, configuration)
   val cloudwatch = new ExtendedCloudWatchClient(provider, configuration)
   val asg = new ExtendedASGClient(provider, configuration)
+  val route53 = new ExtendedRoute53Client(provider, configuration)
 
   val bindings = Map(
     "s3" -> s3,
@@ -21,7 +22,8 @@ class Clients(provider: AWSCredentialsProvider, configuration: ClientConfigurati
     "ec2" -> ec2,
     "rds" -> rds,
     "cloudwatch" -> cloudwatch,
-    "asg" -> asg
+    "asg" -> asg,
+    "route53" -> route53
   )
 
   bindings.foreach { case (name, instance) =>
